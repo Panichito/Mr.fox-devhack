3f1932a4e65defe0a8771445c852aa650bc0b92c
Panichito <panithi2001big@gmail.com>
2023-03-18 00:35:45 +0700

    pub set

--- app/lib/event_register/enrollment.dart
+++ app/lib/event_register/enrollment.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> confirmEnroll(int eid) async {
+  final SharedPreferences pref = await SharedPreferences.getInstance();
+  var myid = pref.getInt('id');
   var url=Uri.https('weatherreporto.pythonanywhere.com', '/api/post-enroll');
+  Map<String, String> header = {"Content-type": "application/json"};
+  String jsondata='{"user":"$myid", "event":"$eid"}';
+  var response = await http.post(url, headers: header, body: jsondata);
+  print('-----result-----');
+  print(response.body);
 }
 
-Future<void> cancelEnroll(int eid, String status) async {
-  var url=Uri.https('weatherreporto.pythonanywhere.com', '/api/delete-enroll/$eid');
+Future<void> cancelEnroll(int eid) async {
+  var enroll_id;
+  var url=Uri.https('weatherreporto.pythonanywhere.com', '/api/delete-enroll/$enroll_id');
 }
